bc3c00dc2937e027b93e599b10cbae861538e0b0
aa <aa@aa.bb>
2016-10-20 20:59:54 +0800

    Initial commit

---
+++ app/src/main/java/com/example/yvtc/wp2016102001/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.yvtc.wp2016102001;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void click1(View v)
+    {
+        Intent it = new Intent();
+        it.setAction(Intent.ACTION_SEND);
+        it.setType("text/plain");
+        it.putExtra(Intent.EXTRA_TEXT,"aba123456");
+        startActivity(it);
+    }
+}
